3d1ba69695b0dfad7148ad38f7786e0a7d05e1b0
Moyo Olojede <Moyo@Moyos-MBP.home>
2021-10-22 19:59:18 -0400

    React was installed into the project and the news api was put in using axios. Search functionality does not work when you try and search a name in the form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { createContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { NewsContextProvider } from './NewsContext';
+import News from "./components/News";
+import './app.css'
 
 function App() {
   return (
     <NewsContextProvider>
-      <div>Hello from app</div>
+      <News />
     </NewsContextProvider>
   );
 }
--- src/NewsContext.js
+++ src/NewsContext.js
@@ -4,21 +4,34 @@ import axios from 'axios';
 export const NewsContext = createContext();
 
 export const NewsContextProvider = (props) => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState('');
   const apiKey = "109e051f09d14e1a8d54c3db2ec827d2";
-
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
   useEffect(() => {
   	axios
   	  .get(
-  	    `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${apiKey}`
+  	    `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}`
   	  )
   	  .then((response) => setData(response.data))
   	  .catch((error) => console.log(error));
-  },[data]);
+  },[]);
 
   return (
-  	<NewsContextProvider value={{ data }}>
+
+  	<NewsContext.Provider value={{ data }}><br />
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            placeholder="Search..."
+            onChange={(e) => setData(e.target.value)}
+          /> 
+          <button type="submit">Enter</button>
+        </form>
+      </div>
   	  {props.children}
-  	</NewsContextProvider>
+  	</NewsContext.Provider>
   );
 };
